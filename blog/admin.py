9945057bbd1be9d7a9054fad42ca1ebc5d90ca94
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-21 00:46:03 -0400

    remove .pyc files

--- blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import Twitter
-
-admin.site.register(Twitter)  #Adding and registering the twitter models
-
